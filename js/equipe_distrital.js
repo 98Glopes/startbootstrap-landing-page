6da32a73f821a0543477b7b85021639b425fcd3a
98Glopes <gabriel.lopes@outlook.com>
2020-07-22 23:43:23 -0300

    add caroussel in equipe distrital gallery

---
+++ js/equipe_distrital.js
@@ -0,0 +1,65 @@
+equipe = [
+    {
+        "title": "Conselho Diretor",
+        "equipe": [
+            {
+                "name": "Victor Padoveze",
+                "thumb": "img/foto_presida/1.jpg",
+                "origin": "img/foto_presida/1000.jpg",
+                "caption": "RDR"
+            },{
+                "name": "Rafael Diniz",
+                "thumb": "img/foto_presida/1.jpg",
+                "origin": "img/foto_presida/1000.jpg",
+                "caption": "Secretário Distrital"
+            },{
+                "name": "Thais",
+                "thumb": "img/foto_presida/1.jpg",
+                "origin": "img/foto_presida/1000.jpg",
+                "caption": "Tesoureira Distrital"
+            },{
+                "name": "Everton",
+                "thumb": "img/foto_presida/1.jpg",
+                "origin": "img/foto_presida/1000.jpg",
+                "caption": "D. Protocolo Distrital"
+            },{
+                "name": "Luiz Fernando Zanuncio",
+                "thumb": "img/foto_presida/1.jpg",
+                "origin": "img/foto_presida/1000.jpg",
+                "caption": "Vice RDR"
+            },
+
+        ]
+    },{
+        "title": "RDAs",
+        "equipe": [
+            {
+                "name": "Victor Padoveze",
+                "thumb": "img/foto_presida/votura.jpg",
+                "origin": "img/foto_presida/1000.jpg",
+                "caption": "RDR"
+            },{
+                "name": "Rafael Diniz",
+                "thumb": "img/foto_presida/votura.jpg",
+                "origin": "img/foto_presida/1000.jpg",
+                "caption": "Secretário Distrital"
+            },{
+                "name": "Thais",
+                "thumb": "img/foto_presida/1.jpg",
+                "origin": "img/foto_presida/1000.jpg",
+                "caption": "Tesoureira Distrital"
+            },{
+                "name": "Everton",
+                "thumb": "img/foto_presida/1.jpg",
+                "origin": "img/foto_presida/1000.jpg",
+                "caption": "D. Protocolo Distrital"
+            },{
+                "name": "Luiz Fernando Zanuncio",
+                "thumb": "img/foto_presida/1.jpg",
+                "origin": "img/foto_presida/1000.jpg",
+                "caption": "Vice RDR"
+            },
+
+        ]
+    },
+  ];
