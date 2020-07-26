07c38bf6c2a8de368128b0de698479c38f254709
98Glopes <gabriel.lopes@outlook.com>
2020-07-26 18:33:12 -0300

    add reorganized presidents page

---
+++ presidentes_json.js
@@ -0,0 +1,165 @@
+// <br />
+presidentes = [
+    {
+        "foto":"Beatriz Zangarelli - Rotaract Club de Len\u00e7\u00f3is Paulista Cidade do Livro.jpeg",
+        "_":"Len\u00e7\u00f3is Paulista Cidade do Livro",
+        "nome":"Beatriz Zangarelli",
+        "clube":"Rotaract Club de Len\u00e7\u00f3is Paulista Cidade do Livro"
+    },
+    {
+        "foto":"Caio Paix\u00e3o - Rotaract Club de Sorocaba-Art Nossa Inova\u00e7\u00e3o.jpeg",
+        "_":"Sorocaba-Art Nossa Inova\u00e7\u00e3o",
+        "nome":"Caio Paix\u00e3o",
+        "clube":"Rotaract Club de Sorocaba-Art Nossa Inova\u00e7\u00e3o"
+    },
+    {
+        "foto":"Camila Prata - Rotaract Club de Piracicaba Aeon.jpeg",
+        "_":"Piracicaba Aeon",
+        "nome":"Camila Prata",
+        "clube":"Rotaract Club de Piracicaba Aeon"
+    },
+    {
+        "foto":"Davi Pereira - Rotaract Club de Piracicaba - Distrito de Tupi.jpeg",
+        "_":"Piracicaba - Distrito de Tupi",
+        "nome":"Davi Pereira",
+        "clube":"Rotaract Club de Piracicaba - Distrito de Tupi"
+    },
+    {
+        "foto":"D\u00e9bora Bruno - Rotaract Club de Americana-A\u00e7\u00e3o.jpg",
+        "_":"Americana-A\u00e7\u00e3o",
+        "nome":"D\u00e9bora Bruno",
+        "clube":"Rotaract Club de Americana-A\u00e7\u00e3o"
+    },
+    {
+        "foto":"Eduardo Lima - Rotaract Club de Itu-Conven\u00e7\u00e3o.jpeg",
+        "_":"Itu-Conven\u00e7\u00e3o",
+        "nome":"Eduardo Lima",
+        "clube":"Rotaract Club de Itu-Conven\u00e7\u00e3o"
+    },
+    {
+        "foto":"Estefan\u00eda Denise - Rotaract Club de Piracicaba.jpeg",
+        "_":"Piracicaba",
+        "nome":"Estefan\u00eda Denise",
+        "clube":"Rotaract Club de <br />Piracicaba"
+    },
+    {
+        "foto":"Gabriela Elias - Rotaract Club de Indaiatuba.jpeg",
+        "_":"Indaiatuba",
+        "nome":"Gabriela Elias",
+        "clube":"Rotaract Club de <br />Indaiatuba"
+    },
+    {
+        "foto":"Gabriel  Sibia - Rotaract Club de Agudos.jpg",
+        "_":"Agudos",
+        "nome":"Gabriel Sibia",
+        "clube":"Rotaract Club de <br />Agudos"
+    },
+    {
+        "foto":"Geraldo Peron - Rotaract Club de Indaiatuba Votura.jpg",
+        "_":"Indaiatuba Votura",
+        "nome":"Geraldo Peron",
+        "clube":"Rotaract Club de Indaiatuba Votura"
+    },
+    {
+        "foto":"Guilherme Cunha - Rotaract Club de Santa Cruz do Rio Pardo.jpeg",
+        "_":"Santa Cruz do Rio Pardo",
+        "nome":"Guilherme Cunha",
+        "clube":"Rotaract Club de Santa Cruz do Rio Pardo"
+    },
+    {
+        "foto":"Jhenifer Gon\u00e7alves - Rotaract Club de Len\u00e7\u00f3is Paulista.jpg",
+        "_":"Len\u00e7\u00f3is Paulista",
+        "nome":"Jhenifer Gon\u00e7alves",
+        "clube":"Rotaract Club de Len\u00e7\u00f3is Paulista"
+    },
+    {
+        "foto":"Lais Onora - Rotaract Club de Salto Moutonn\u00e9e.jpeg",
+        "_":"Salto Moutonn\u00e9e",
+        "nome":"La\u00eds Onora",
+        "clube":"Rotaract Club de Salto Moutonn\u00e9e"
+    },
+    {
+        "foto":"Larissa Kiyanitza - Rotaract Club de Americana.jpeg",
+        "_":"Americana",
+        "nome":"Larissa Kiyanitza",
+        "clube":"Rotaract Club de <br />Americana"
+    },
+    {
+        "foto":"Leonardo Oliveira - Rotaract Club de Cap\u00e3o Bonito.jpg",
+        "_":"Cap\u00e3o Bonito",
+        "nome":"Leonardo Oliveira",
+        "clube":"Rotaract Club de Cap\u00e3o Bonito"
+    },
+    {
+        "foto":"Leticia Valli - Rotaract Club Luiz de Queiroz.jpeg",
+        "_":"Luiz de Queiroz",
+        "nome":"Leticia Valli",
+        "clube":"Rotaract Club Luiz de Queiroz"
+    },
+    {
+        "foto":"Lucas Ferreira - Rotaract Club de Macatuba.jpeg",
+        "_":"Macatuba",
+        "nome":"Lucas Ferreria",
+        "clube":"Rotaract Club de <br />Macatuba"
+    },
+    {
+        "foto":"Mariana Bastos - Rotaract Club de Tatu\u00ed.jpeg",
+        "_":"Tatu\u00ed",
+        "nome":"Mariana Bastos",
+        "clube":"Rotaract Club de <br />Tatu\u00ed"
+    },
+    {
+        "foto":"Matheus Felipe - Rotaract Club de Rio das Pedras.jpeg",
+        "_":"Rio das Pedras",
+        "nome":"Matheus Felipe",
+        "clube":"Rotaract Club de Rio das Pedras"
+    },
+    {
+        "foto":"Paulo Pavani -Rotaract Club de Sorocaba Leste.jpeg",
+        "_":"Sorocaba Leste",
+        "nome":"Paulo Pavani",
+        "clube":"Rotaract Club de Sorocaba-Leste"
+    },
+    {
+        "foto":"Raphael Pettazzoni - Rotaract Club de S\u00e3o Manuel.jpeg",
+        "_":"S\u00e3o Manuel",
+        "nome":"Raphael Pettazoni",
+        "clube":"Rotaract Club de S\u00e3o Manuel"
+    },
+    {
+        "foto":"Richard Michael - Rotaract Club de Cabre\u00fava.jpeg",
+        "_":"Cabre\u00fava",
+        "nome":"Richard Michael",
+        "clube":"Rotaract Club de <br />Cabre\u00fava"
+    },
+    {
+        "foto":"Ruan Lucas - Rotaract Club de Boituva-Sul.jpeg",
+        "_":"Boituva-Sul",
+        "nome":"Ruan Lucas",
+        "clube":"Rotaract Club de Boituva-Sul"
+    },
+    {
+        "foto":"Tatiana Alves - Rotaract Club de Sorocaba-Vergueiro.jpg",
+        "_":"Sorocaba-Vergueiro",
+        "nome":"Tatiana Alves",
+        "clube":"Rotaract Club de Sorocaba-Vergueiro"
+    },
+    {
+        "foto":"Tiago Almeida - Rotaract Club de Itapeva.jpeg",
+        "_":"Itapeva",
+        "nome":"Tiago Almeida",
+        "clube":"Rotaract Club de <br />Itapeva"
+    },
+    {
+        "foto":"Vanessa da Silva - Rotaract Club de Botucatu Bons Ares.jpeg",
+        "_":"Botucatu Bons Ares",
+        "nome":"Vanessa da Silva",
+        "clube":"Rotaract Club de Botucatu Bons Ares"
+    },
+    {
+        "foto":"Vitor Antunes - Rotaract Club de Sorocaba-Manchester.jpg",
+        "_":"Sorocaba-Manchester",
+        "nome":"Vitor Antunes",
+        "clube":"Rotaract Club de Sorocaba-Manchester"
+    }
+]
